64ddc92221850a609da99e08ff82fc6a2ed297f7
yanmingjun <195945056@qq.com>
2016-01-09 15:54:45 +0800

    haha

---
+++ YMJLunarDatePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YMJLunarDatePicker
+//
+//  Created by yanmingjun on 16/1/9.
+//  Copyright © 2016年 youloft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
